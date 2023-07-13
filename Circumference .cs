c8ec9f899e583554827c598dbe85ad9bfecc390b
nadka <nadka@LAPTOP-8FCMH9F4>
2023-07-13 22:26:31 +0300

    Добавьте файлы проекта.

---
+++ Circumference .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiguresLibrary
+{
+    public class Circumference : Figure
+    {
+        public double radius;
+        public Circumference(double rad)
+        {
+            radius = rad;
+        }
+        public override double GetArea()
+        {
+            Area = 3.14 * Math.Pow(radius, 2);
+            return Area;
+        }
+        public override double GetPerim()
+        {
+            Perim = 2 * 3.14 * radius;
+            return Perim;
+        }
+    }
+}
---
+++ Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FiguresLibrary
+{
+    public class Figure
+    {
+        public double Area;
+        public double Perim;
+        public virtual double GetArea()
+        {
+            return Area;
+        }
+        public virtual double GetPerim()
+        {
+            return Perim;
+        }
+
+        public void getInfomation()
+        {
+            Console.WriteLine($"Площадь: {Math.Round(GetArea(), 2)}");
+            Console.WriteLine($"Периметр: {Math.Round(GetPerim(), 2)}");
+        }
+
+    }
+}
+
